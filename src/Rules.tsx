011d6b1e9f4eecb5753edbd4047807cee3a50766
eronth <github@nicpereira.dev>
2024-10-14 10:16:09 -0500

    Initialize project.

---
+++ src/Rules.tsx
@@ -0,0 +1,15 @@
+function Rules() {
+  return (
+    <div>
+      <h1>Hellgrinder</h1>
+      <p>Here are the rules of the game:</p>
+      <ul>
+        <li>Rule 1</li>
+        <li>Rule 2</li>
+        <li>Rule 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Rules;

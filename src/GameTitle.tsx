19349c5b7a8a4ea47b4ca70210dce10bb1715041
eronth <github@nicpereira.dev>
2024-10-17 02:52:13 -0500

    More font adds and cleanups.

--- src/GameTitle.tsx
+++ src/GameTitle.tsx
@@ -31,12 +31,14 @@ export default function GameTitle() {
       <h1 className={css} title="Hellgrinder">
         
         {isPreOver ? pre : null}
-        <span className='title'>
+        <span className='inline-title'>
           {isPreOver ? null : <>{pre}&nbsp;</>}
 
-          <span className='first-letter'>{firstLetter}</span>
-          {middleLetters}
-          <span className='final-letter'>{finalLetter}</span>
+          <span className='title'>
+            <span className='first-letter'>{firstLetter}</span>
+            {middleLetters}
+            <span className='final-letter'>{finalLetter}</span>
+          </span>
 
           {isPostUnder ? null : <>&nbsp;{post}</>}
         </span>
@@ -48,52 +50,54 @@ export default function GameTitle() {
 
   const titlesDesigns: JSX.Element[] = [
     title('lancelot', {pre: 'The Tales of', above: true}, null),
-    //title('lancelot', {pre:'A'}, {post:'Story'}, "Hellgrinder's"),
-    // title('eagle-lake', {pre: 'The', over: true}, {post:'Saga', below: true}),
-    // title('girassol', {pre:'The'}, {post:'Chronicles'}),
-    // title('rye'),
-    // title('cutive-mono-regular', null, {post: 'Files.'}, ),
-    // title('goblin-one-regular', {pre: "It's the", above: true}, {post: "Show"}),
-    // title('noto-serif-oriya-100', null, null, 'Hellgrinder.'),
-    // title('emilys-candy-regular', {pre: 'A'}, {post: 'World'}, "Hellgrinder's"),
-    // title('life-savers-regular', {pre: 'My'}, {post: 'Life'}),
-    // title('life-savers-bold', {pre: 'My'}, {post: 'Life'}),
-    // title('life-savers-extrabold', {pre: 'My'}, {post: 'Life'}),
-    // title('spicy-rice-regular', {pre: 'That', above: true}, {post: 'Show', below: true}),
-    // title('cinzel-400', {pre: 'The Story of', above: true}),
-    // title('quintessential-regular', {pre: 'Once Upon A', above: true}),
-    // title('eater-regular', {pre: 'The Unholy', above: true}),
-    // title('fontdiner-swanky-regular', {pre: 'The Grand \'ol', above: true}),
-    // title('risque-regular', {pre: '✶'}, {post: '✶'}),
-    // title('kalnia-100', {pre: 'That\'s'}, null, 'Hellgrinder!'),
-    // title('croissant-one-regular', {pre: 'A'}, {post: 'for Two'}),
-    // title('cinzel-decorative-regular', {pre: 'The end of', above: true}, null, 'hellgrinder'),
-    // title('cinzel-decorative-regular2', null, null, 'HellgrindeR'),
-    // title('metamorphous-regular', null, {post: 'Oracle'}),
-    // title('shrikhand-regular', {pre: "What about", above: true}, null, 'Hellgrinder?'),
-    // title('cormorant-sc-light', null, null, "Hell's Grinder"),
-    // title('cormorant-unicase-light'),
-    // title('della-respira-regular', {pre: 'The Unending Torment of', above: true}),
-    // title('sedan-sc-regular', {pre: 'The Tome of', above: true}, null, 'HellgrindeR'),
-    // title('castoro-titling-regular'),
-    // title('kranky-regular', {pre: 'The'}, {post: 'Companion'}, 'Hellgrinder\'s'),
-    // title('diplomata-sc-regular', null, null, 'Hell and Grinder'),
-    // title('elsie-swash-caps-regular', {pre: 'A'}, {post: 'Romance'}, "Hellgrinder"),
-    // title('almendra-regular',
-    //   null,
-    //   {post: 'When the toils of Hell doth grind thee unto dust, '
-    //     + 'thou shalt, henceforth and in kind, '
-    //     + 'grind hell beneath thine own heel.', below: true},
-    //   'Hellgrinder'),
-    // title('federant-regular', {pre: 'To'}, {post: 'and Back'}),
-    // title('gideon-roman-regular', {pre: 'The Depths of'}),
-    // title('anton-sc-regular', null, null, 'HellgrindeR'),
-    // title('almendra-display-regular', {pre: 'Musings on'}),
-    // title('chonburi-regular', null, null, 'Hellgrinder.'),
-    // title('almendra-sc-regular', {pre: 'A Companion Guide on how to survive your dive into', above: true}, null, 'The Hellgrinder'),
-    // title('bebas-neue-regular', null, null, 'Hellgrinder'),
-    // title('merriweather-light', {pre: 'How to'}, null, 'Grind your Hell'),
-
+    title('lancelot2', {pre:'A'}, {post:'Story'}, "Hellgrinder's"),
+    title('eagle-lake', {pre: 'The', over: true}, {post:'Saga', below: true}),
+    title('girassol', {pre:'The'}, {post:'Chronicles'}),
+    title('rye'),
+    title('cutive-mono-regular', null, {post: 'Files.'}, ),
+    title('goblin-one-regular', {pre: "It's the", above: true}, {post: "Game"}),
+    title('noto-serif-oriya-100', null, null, 'Hellgrinder.'),
+    title('emilys-candy-regular', {pre: 'A'}, {post: 'World'}, "Hellgrinder's"),
+    title('life-savers-regular', {pre: 'My'}, {post: 'Life'}),
+    title('life-savers-bold', {pre: 'My'}, {post: 'Life'}),
+    title('life-savers-extrabold', {pre: 'My'}, {post: 'Life'}),
+    title('spicy-rice-regular', {pre: 'That', above: true}, {post: 'Game', below: true}),
+    title('cinzel-400', {pre: 'The Story of', above: true}),
+    title('quintessential-regular', {pre: 'Once Upon A', above: true}),
+    title('eater-regular', {pre: 'The Unholy', above: true}),
+    title('fontdiner-swanky-regular', {pre: 'The Grand \'ol', above: true}),
+    title('risque-regular', {pre: '✶'}, {post: '✶'}),
+    title('kalnia-100', {pre: 'That\'s'}, null, 'Hellgrinder!'),
+    title('croissant-one-regular', {pre: 'A'}, {post: 'for Two'}),
+    title('cinzel-decorative-regular', {pre: 'The end of', above: true}, null, 'hellgrinder'),
+    title('cinzel-decorative-regular2', null, null, 'HellgrindeR'),
+    title('metamorphous-regular', null, {post: 'Oracle'}),
+    title('shrikhand-regular', {pre: "What about", above: true}, null, 'Hellgrinder?'),
+    title('cormorant-sc-light', null, null, "Hell's Grinder"),
+    title('cormorant-unicase-light'),
+    title('della-respira-regular', {pre: 'The Unending Torment of', above: true}),
+    title('sedan-sc-regular', {pre: 'The Tome of', above: true}, null, 'HellgrindeR'),
+    title('castoro-titling-regular'),
+    title('kranky-regular', {pre: 'The'}, {post: 'Pocketbook'}, 'Hellgrinder\'s'),
+    title('diplomata-sc-regular', null, null, 'Hell and Grinder'),
+    title('elsie-swash-caps-regular', {pre: 'A'}, {post: 'Romance'}, "Hellgrinder"),
+    title('almendra-regular',
+      null, 
+      {
+        post: 'When the toils of Hell doth grind thee unto dust, '
+          + 'thou shalt, henceforth and in kind, grind hell beneath thine own heel.',
+        below: true
+      },
+      'Hellgrinder'),
+    title('federant-regular', {pre: 'To'}, {post: 'and Back'}),
+    title('gideon-roman-regular', {pre: 'The Depths of'}),
+    title('anton-sc-regular', null, null, 'HellgrindeR'),
+    title('almendra-display-regular', {pre: 'Musings on'}),
+    title('chonburi-regular', null, null, 'Hellgrinder.'),
+    title('almendra-sc-regular', {pre: 'A Companion Guide on how to survive your dive into', above: true}, null, 'The Hellgrinder'),
+    title('bebas-neue-regular', null, null, 'Hellgrinder'),
+    title('merriweather-light', {pre: 'How to'}, null, 'Grind your Hell'),
+    title('roboto-regular', {pre: 'Like, Comment, ', above: true}, null, 'and Hellgrind'),
   ];
 
   return (<span className='title-region'>{getRandElement(titlesDesigns)}</span>);

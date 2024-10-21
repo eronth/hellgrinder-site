03b43ec6c208b0432c2e8a09f472b80a7f56805f
eronth <github@nicpereira.dev>
2024-10-21 01:46:28 -0500

    Equipment filler.

--- src/pages/AdditionalEquipmentPage.tsx
+++ src/pages/AdditionalEquipmentPage.tsx
@@ -1,6 +1,7 @@
 import GameTitle from "../GameTitle";
 import NavTabs from "../common-design/nav/NavTabs";
 import { TabType } from "../ts-types/types";
+import Tools from "../common-design/Tools";
 
 export default function AdditionalEquipmentPage() {
   const page: TabType = 'additional-equipment';
@@ -8,6 +9,8 @@ export default function AdditionalEquipmentPage() {
   return (<div className={page}>
     <GameTitle />
     <NavTabs selectedTab={page} />
+    <hr />
+    <p>{Tools.getLoremIpsum()}</p>
     <hr />
   </div>);
 }

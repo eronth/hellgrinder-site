0633325dce1a6e7b495010ba286ad031f5b398c8
eronth <github@nicpereira.dev>
2024-11-19 15:00:27 -0600

    better sort for items, printing out perks now.

--- src/pages/AdditionalEquipmentPage.tsx
+++ src/pages/AdditionalEquipmentPage.tsx
@@ -31,29 +31,54 @@ export default function AdditionalEquipmentPage() {
     <p>
       The equipment listed on this page is not meant for players to start with. Instead, it is a list of
       equipment to be given out as rewards or found in the world. This equipment is often meant to be more
-      powerful than some of the standard equipment.
+      powerful than some of the standard equipment. Some equipment is considered "Advanced" equipment,
+      while it requires no special training to use, it should be rewarded less often than other equipment.
     </p>
     <hr />
     <h2>Weapons</h2>
     
     <h3>Melee Weapons</h3>
+    <h4>Basic Melee Weapons</h4>
     <div className={'col-handler'}>
       {sortedMeleeWeapons.map((w, i) =>
-        <WeaponComponent key={`melee-weapon-${i}`} weapon={w} />)
+        w.isAdvancedItem ? null : <WeaponComponent key={`basic-melee-weapon-${i}`} weapon={w} />)
+      }
+    </div>
+
+    <h4>Advanced Melee Weapons</h4>
+    <div className={'col-handler'}>
+      {sortedMeleeWeapons.map((w, i) =>
+        w.isAdvancedItem ? <WeaponComponent key={`advanced-melee-weapon-${i}`} weapon={w} /> : null)
       }
     </div>
     
     <h3>Shooting Weapons</h3>
+    <h4>Basic Shooting Weapons</h4>
     <div className={'col-handler'}>
       {sortedShootingWeapons.map((w, i) =>
-        <WeaponComponent key={`shooting-weapon-${i}`} weapon={w} />)
+        w.isAdvancedItem ? null : <WeaponComponent key={`basic-shooting-weapon-${i}`} weapon={w} />)
+      }
+    </div>
+
+    <h4>Advanced Shooting Weapons</h4>
+    <div className={'col-handler'}>
+      {sortedShootingWeapons.map((w, i) =>
+        w.isAdvancedItem ? null : <WeaponComponent key={`advanced-shooting-weapon-${i}`} weapon={w} />)
       }
     </div>
     
     <h3>Thrown Weapons</h3>
+    <h4>Basic Thrown Weapons</h4>
     <div className={'col-handler'}>
       {sortedThrownWeapons.map((w, i) =>
-        <WeaponComponent key={`thrown-weapon-${i}`} weapon={w} />)
+        w.isAdvancedItem ? null : <WeaponComponent key={`basic-thrown-weapon-${i}`} weapon={w} />)
+      }
+    </div>
+
+    <h4>Advanced Thrown Weapons</h4>
+    <div className={'col-handler'}>
+      {sortedThrownWeapons.map((w, i) =>
+        w.isAdvancedItem ? <WeaponComponent key={`advanced-thrown-weapon-${i}`} weapon={w} /> : null)
       }
     </div>
     
@@ -64,12 +89,22 @@ export default function AdditionalEquipmentPage() {
       you can use to learn the spell.
       Once learned, spells act much like weapons, able to be equipped in the same way and used to make attacks.
     </p>
+    <h4>Basic Arcane Weapons</h4>
     <div className={'col-handler'}>
       {sortedArcaneWeapons.map((w, i) =>
-        <WeaponComponent key={`spell-${i}`} weapon={w} />)
+        w.isAdvancedItem ? null : <WeaponComponent key={`basic-spell-${i}`} weapon={w} />)
+      }
+    </div>
+
+    <h4>Advanced Arcane Weapons</h4>
+    <div className={'col-handler'}>
+      {sortedArcaneWeapons.map((w, i) =>
+        w.isAdvancedItem ? <WeaponComponent key={`advanced-spell-${i}`} weapon={w} /> : null)
       }
     </div>
     
+    <hr />
+    <h2>Wands and Staves</h2>
     <h3>Wands and Staves</h3>
     <p>
       Wands and staves can be used to augment spell tomes, gems, and attacks. When you equip a wand or staff,
@@ -84,24 +119,45 @@ export default function AdditionalEquipmentPage() {
       <li>Grant a bonus +1d4 damage to attacks made with the staff.</li>
       <li>Remove the [One-handed] tag and add the [Two-handed] tag.</li>
     </ul>
+
+    <h4>Basic Wands and Staves</h4>
     <div className={'col-handler'}>
       {sortedWandsAndStaves.map((w, i) =>
         <ItemComponent key={`wand-${i}`} item={w} />)
       }
     </div>
     
+    <hr />
+    <h2>Protection</h2>
     <h3>Armor</h3>
+    <h4>Basic Armor</h4>
     <div className={'col-handler'}>
       {sortedArmor.map((w, i) =>
-        <ItemComponent key={`armor-${i}`} item={w} />)
+        w.isAdvancedItem ? null: <ItemComponent key={`basic-armor-${i}`} item={w} />)
+      }
+    </div>
+
+    <h4>Advanced Armor</h4>
+    <div className={'col-handler'}>
+      {sortedArmor.map((w, i) =>
+        w.isAdvancedItem ? <ItemComponent key={`advanced-armor-${i}`} item={w} /> : null)
       }
     </div>
     
-    
+    <hr />
+    <h2>Other Gear</h2>
     <h3>Gear</h3>
+    <h4>Basic Gear</h4>
     <div className={'col-handler'}>
       {sortedGear.map((w, i) =>
-        <ItemComponent key={`gear-${i}`} item={w} />)
+        w.isAdvancedItem ? null : <ItemComponent key={`basic-gear-${i}`} item={w} />)
+      }
+    </div>
+
+    <h4>Advanced Gear</h4>
+    <div className={'col-handler'}>
+      {sortedGear.map((w, i) =>
+        w.isAdvancedItem ? <ItemComponent key={`advanced-gear-${i}`} item={w} /> : null)
       }
     </div>
     
--- src/pages/AdvancedPerksPage.tsx
+++ src/pages/AdvancedPerksPage.tsx
@@ -1,13 +1,41 @@
 import GameTitle from "../GameTitle";
 import NavTabs from "../common-design/nav/NavTabs";
 import { TabType } from "../ts-types/types";
+import Tools from "../common-design/Tools";
+import Perks from "../common-design/equipment/perks";
+import PerkComponent from "./character-creation-components/perks/PerkComponent";
 
 export default function AdvancedPerksPage() {
   const page: TabType = 'advanced-perks';
+
+  const sortedPerks = Tools.sortPerks(Perks);
 
   return (<div className={'page ' + page}>
     <GameTitle />
     <NavTabs selectedTab={page} />
     <hr />
+    <h2>Perks</h2>
+    <p>
+      Perks are special abilities that can be used to enhance your character. Some perks are considered starter perks,
+      and can be taken during character creation (if you have hte points for them) or during character advancement. Other
+      perks are considered advancement perks, and should only be taken during character advancement. Perks can be taken in
+      any order, but some perks may have prerequisites that must be met before they can be taken.
+    </p>
+
+    <h4>Starter Perks</h4>
+    <div className={'col-handler'}>
+      {sortedPerks.map((p, i) =>
+        p.isAdvancedItem ? null : <PerkComponent key={`starter-perk-${i}`} perk={p} />)
+      }
+    </div>
+
+    <h4>Advancement Perks</h4>
+    <div className={'col-handler'}>
+      {sortedPerks.map((p, i) =>
+        p.isAdvancedItem ? <PerkComponent key={`basic-perk-${i}`} perk={p} /> : null)
+      }
+    </div>
+
+
   </div>);
 }

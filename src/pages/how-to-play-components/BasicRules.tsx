2731dd57d1f575c2685e9c8b190d486d738a037c
Nic Pereira <github@nicpereira.dev>
2025-06-09 11:32:59 -0500

    Fix some visuals on smaller screen.

--- src/pages/how-to-play-components/BasicRules.tsx
+++ src/pages/how-to-play-components/BasicRules.tsx
@@ -155,12 +155,12 @@ export default function BasicRules() {
       </tr>
       <tr>
         <td>4-7</td>
-        <td>Rank 0</td>
+        <td>Rank&nbsp;0</td>
         <td>You fail at whatever you're attempting to accomplish.</td>
       </tr>
       <tr>
         <td>8-11</td>
-        <td>Rank 1</td>
+        <td>Rank&nbsp;1</td>
         <td>
           You succeed at a minor level. You partially succeed at your efforts, or some complications
           that arise with the success.
@@ -168,14 +168,14 @@ export default function BasicRules() {
       </tr>
       <tr>
         <td>12-15</td>
-        <td>Rank 2</td>
+        <td>Rank&nbsp;2</td>
         <td>
           You succeed at your task. There are no extra complications from your efforts.
         </td>
       </tr>
       <tr>
         <td>16-18</td>
-        <td>Rank 3</td>
+        <td>Rank&nbsp;3</td>
         <td>
           You succeed at your task with a flourish. You often gain extra benefits for your efforts, or
           a bonus to your next Skill Check.

7fe4371bdda1f7acd0159303daceb7154d687c3a
Nic Pereira <github@nicpereira.dev>
2025-10-01 05:37:15 -0500

    More Zephpter detail

--- src/pages/story-and-settings-components/settings-components/demon-clans/ZephpterHorde.tsx
+++ src/pages/story-and-settings-components/settings-components/demon-clans/ZephpterHorde.tsx
@@ -13,14 +13,23 @@ export default function ZephpterHorde ({ hx }: Props) {
     <DemonClanComponent title={"Zephpter Hordes"} hx={hx} buttonText={buttonText}>
       <p>
         The Zephpter Hordes are filled with nethercurrent-aligned demonic critters that reside
-        along the walls of the Zephyrian Spires. They are small, insect-like creatures
-        that can fly through the lingering clouds as a bolt of lightning.
+        along the walls of the Zephyrian Spires. They are small, insect-like creatures with
+        savage, lightning-fast movements and a penchant for swarming their prey.
+        The Zephpter can fly through the lingering clouds as a bolt of lightning, allowing them
+        to quickly close in on their prey. If an unfortunate soul is caught in a Zephpter
+        storm, they are struck with hundreds of bolts of lightning which instantly transform
+        into a frenzied Zephpter swarm.
       </p>
       <p>
         The Zephpter Hordes are known for their chaotic nature, often swarming in large numbers
         and attacking anything that disturbs their territory. They are drawn to sources of
         energy, such as electrical devices or magical artifacts, and will fiercely defend
         these sources from intruders. Their small size and agility make them difficult to catch.
+      </p>
+      <p>
+        Each Zephpter Horde mindlessly follows a titanic storm — churning and swirling with nethercurrent.
+        Scholars debate the sentience of these
+        storms, but all agree that they are a force of hell to be reckoned with. 
       </p>
     </DemonClanComponent>
   )

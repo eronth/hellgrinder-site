873d8f6296b30e7c50c8d817fe5b5c5a863a819f
Nic Pereira <github@nicpereira.dev>
2025-10-01 03:19:06 -0500

    Cleaner stoneveined description.

--- src/pages/story-and-settings-components/settings-components/demon-clans/StoneveinedOrder.tsx
+++ src/pages/story-and-settings-components/settings-components/demon-clans/StoneveinedOrder.tsx
@@ -23,10 +23,25 @@ export default function StoneveinedOrder ({ hx }: Props) {
       </p>
       <p>
         The Stoneveined move slowly and deliberately, as if in tune
-        with the deep, tectonic forces of hell. Their motives are obscure,
+        with the deep, tectonic forces of hell. As they move, the scraping
+        and grinding of stone echoes through the caverns, in a dissonant
+        yet somehow harmonic melody that instills a sense
+        of unease for all who hear it. Their motives are obscure,
         even to other infernal factions, but their presence is
         undeniable—wherever they appear, hell itself seems to shift and
-        warp. It is said that the Stoneveined Order has existed since
+        warp.
+      </p>
+      <p>
+        When Lords of the Stoneveined Choir move, they move deliberately
+        and without notice of those around them. Their footsteps
+        cause the very ground to tremble, and the air is filled with
+        their rackerous song. Though they are not inherently hostile,
+        if they are attacked or hindered, pieces of the Lord and the world 
+        will spring to life to assault the offenders. It is as if they are
+        a natural defense of the Lords.
+      </p>
+      <p>
+        It is said that the Stoneveined Order has existed since
         the very creation of hell, rumored to serve as caretakers of
         the realm itself. Yet some rumors have whispers of an even darker
         purpose for these soldiers.

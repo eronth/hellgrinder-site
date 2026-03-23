78d2c81820a30995b1453e5cca445c7c42d71629
Nic Pereira <github@nicpereira.dev>
2026-03-23 17:55:55 -0500

    more gear

--- src/common-design/equipment/gear.tsx
+++ src/common-design/equipment/gear.tsx
@@ -19,7 +19,37 @@ const exObj: { [key: string]: Item } = {
     tags: ['Footwear'],
     isAdvancedItem: true,
     effects: ['You can spend one Movement Point to ignore difficult and hazardous terrain for that turn.'],
-  }
+  },
+  sinnersCompass: {
+    name: 'Sinner\'s Compass',
+    tags: [],
+    isAdvancedItem: false,
+    effects: ['Points toward the nearest source of suffering. Not always helpful.'],
+  },
+  luckyCoin: {
+    name: 'Lucky Coin',
+    tags: [],
+    isAdvancedItem: false,
+    effects: ['Once per day, you can flip this coin. Call it in the air. If you\'re right, increase or decrease the Success Rank of the next roll by 1. If you\'re wrong, nothing happens.'],
+  },
+  smokeVial: {
+    name: 'Smoke Vial',
+    tags: ['Consumable'],
+    isAdvancedItem: false,
+    effects: ['As an action, smash this vial to create a 10-foot radius of obscuring smoke for 1 minute.'],
+  },
+  // fleshWeaveCloak: {
+  //   name: 'Flesh Weave Cloak',
+  //   tags: ['Torso', 'Heavy'],
+  //   isAdvancedItem: true,
+  //   effects: ['Once per day, you can pull the cloak tight around yourself to heal 1d6 HP. The cloak feeds on your fear to mend your wounds.'],
+  // },
+  // boneWhistle: {
+  //   name: 'Bone Whistle',
+  //   tags: ['Arcane'],
+  //   isAdvancedItem: true,
+  //   effects: ['When blown, all creatures within 30 feet must succeed on a Will save or become unnerved, giving them disadvantage on their next attack.'],
+  // },
 };
 
 export default exObj;
